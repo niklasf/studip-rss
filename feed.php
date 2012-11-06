2c8343fa637e75749b3ac217934704c5ffd33698
Niklas Fiekas <niklas.fiekas@googlemail.com>
2012-11-06 17:35:06 +0100

    cron.php -> cron.inc.

--- feed.php
+++ feed.php
@@ -10,7 +10,7 @@ header("Content-Type: application/rss+xml");
 if (!file_exists('feed.xml') || filemtime('feed.xml') < time() - 60 * 5) {
   // Refresh the cache.
   ob_start();
-  require 'cron.php';
+  require 'cron.inc';
   file_put_contents('feed.xml', ob_get_contents());
   ob_end_flush();
 }
