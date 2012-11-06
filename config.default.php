c61d47ec17271ab79f226cd95567e239443972e0
Niklas Fiekas <niklas.fiekas@googlemail.com>
2012-11-06 17:44:27 +0100

    Add pubDates for the items.

--- config.default.php
+++ config.default.php
@@ -1,7 +1,7 @@
 <?php
 
 define('STUDIP_RSS_TEMPDIR', '/tmp');
-define('STUDIP_RSS_USER', 'RZ-Kürzel');
-define('STUDIP_RSS_PASS', 'RZ-Passwort');
+define('STUDIP_RSS_USER', 'Username');
+define('STUDIP_RSS_PASS', 'Password');
 define('STUDIP_RSS_SOURCE', 'https://studip.tu-clausthal.de/');
 define('STUDIP_RSS_BASE', 'http://localhost/studip-rss/');
