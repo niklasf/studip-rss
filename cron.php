d5805623b17eb348b50d76b04d0c18a35f290094
Niklas Fiekas <niklas.fiekas@googlemail.com>
2012-11-06 16:43:47 +0100

    Try superentities.

--- cron.php
+++ cron.php
@@ -3,6 +3,26 @@
 error_reporting(E_ALL);
 require 'config.php';
 
+function superentities( $str ){
+    // get rid of existing entities else double-escape
+    //$str = html_entity_decode(stripslashes($str),ENT_QUOTES,'UTF-8'); 
+    $str2 = '';
+    $ar = preg_split('/(?<!^)(?!$)/u', $str );  // return array of every multi-byte character
+    foreach ($ar as $c){
+        $o = ord($c);
+        if ( (strlen($c) > 1) || /* multi-byte [unicode] */
+            ($o <32 || $o > 126) || /* <- control / latin weirdos -> */
+            ($o >33 && $o < 40) ||/* quotes + ambersand */
+            ($o >59 && $o < 63) /* html */
+        ) {
+            // convert to numeric entity
+            $c = mb_encode_numericentity($c,array (0x0, 0xffff, 0, 0xffff), 'UTF-8');
+        }
+        $str2 .= $c;
+    }
+    return $str2;
+}
+
 // Create a cookie file.
 $cookie_file = tempnam(STUDIP_RSS_TEMPDIR, "studip-rss-");
 
@@ -41,7 +61,7 @@ print "<rss version=\"2.0\">\n";
 print "  <channel>\n";
 print "    <title>StudIP</title>\n";
 print "    <link>" . STUDIP_RSS_SOURCE . "index.php?again=yes</link>\n";
-print "    <description>Dateien aus dem StudIP.</description>\n";
+print "    <description>Dateien aus dem StudIP</description>\n";
 print "    <lastBuildDate>" . date('r') . "</lastBuildDate>\n";
 print "    <pubDate>" . date('r') . "</pubDate>\n";
 print "    <image>\n";
@@ -62,7 +82,7 @@ preg_match_all('/\"seminar_main\.php\?auswahl=([0-9a-f]+)\".*?>(.*?)<\/a>/s', $r
 // Iterate over all seminars.
 foreach ($matches as $match) {
   $auswahl = $match[1];
-  $seminar = html_entity_decode(trim($match[2]));
+  $seminar = superentities(html_entity_decode(trim($match[2])));
 
   // Load the main seminar page.
   $req = curl_init(STUDIP_RSS_SOURCE . "seminar_main.php?auswahl=" . urlencode($auswahl));
@@ -107,7 +127,7 @@ foreach ($matches as $match) {
       $description = "";
 
       print "    <item>\n";
-      print "      <title>[$seminar] " . html_entity_decode($match[2]) . "</title>\n";
+      print "      <title>[$seminar] " . superentities(html_entity_decode($match[2])) . "</title>\n";
       print "      <link>" . STUDIP_RSS_BASE . $filename . "</link>\n";
       print "      <guid isPermaLink=\"false\">" . $match[1] . "</guid>\n";
       print "      <enclosure url=\"" . STUDIP_RSS_BASE . $filename . "\" length=\"" . $filesize . "\" type=\"" . $filetype . "\" />\n";
